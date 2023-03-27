98e155a5123d04f89ab138a7c755ff84a0c449f8
Sanghun Jo <sanghunjo@Sanghunui-MacBookPro.local>
2023-03-27 22:36:28 +0900

    implemented views.py about Property

--- realtor/properties/serializers.py
+++ realtor/properties/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
-from .models import Property, PreferredProperty
-from properties.enums import PropertyType, DetailedType, StatusType
+
+from .models import Property
 
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
-        fields = ['id', 'image', 'price', 'city', 'district', 'zone', 'property_type',
-                  'detailed_type', 'size', 'description', 'maintenance_cost', 'status']
+        fields = [
+            "id",
+            "image",
+            "price",
+            "city",
+            "district",
+            "zone",
+            "property_type",
+            "detailed_type",
+            "size",
+            "description",
+            "maintenance_cost",
+            "status",
+        ]
--- realtor/properties/urls.py
+++ realtor/properties/urls.py
@@ -2,10 +2,9 @@ from django.urls import include, path
 from properties import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-
 urlpatterns = [
-    path("properties/", views.PropertyList.as_view()),
-    path('properties/<int:pk>/', views.PropertyDetail.as_view())
+    path("", views.PropertyList.as_view()),
+    path("<int:pk>/", views.PropertyDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
