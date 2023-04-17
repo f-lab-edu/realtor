4f7953223ed3208c1c7b2b1537cc3cd8a4dc1bdc
Sanghun Jo <sanghunjo@Sanghunui-MacBookPro.local>
2023-04-17 18:02:23 +0900

    defined useradmin & propertyadmin

--- realtor/users/admin.py
+++ realtor/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from rangefilter.filter import DateRangeFilter
+
+from .models import User
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "upper_case_name", "date_joined", "last_login"]
+    list_editable = ["username"]
+    list_per_page = 5
+    list_filter = ("username", ("date_joined", DateRangeFilter))
+    exclude = ["password"]
+    search_fields = ["username", "first_name", "last_name"]
+
+    @admin.display(description="Name")
+    def upper_case_name(self, obj):
+        return ("%s %s" % (obj.first_name, obj.last_name)).upper()
