d64eda54e55f1435bbe827d09a6ec45cde630ec3
sanghunjo921 <salr921@gmail.com>
2023-05-09 10:58:00 +0900

    practice validation using pytest parametrizing

--- realtor/test/test_authentication.py
+++ realtor/test/test_authentication.py
@@ -20,13 +20,31 @@ def register_user(db, django_user_model):
     return make_user
 
 
+@pytest.fixture
+def auto_login_user(db, client, register_user):
+    def make_auto_login(user=None):
+        if user is None:
+            user = register_user()
+        client.login(username=user.username, password="wjddk12wjddk")
+        return client, user
+
+    return make_auto_login
+
+
 def test_unauthorized(client):
     response = client.get("/admin/")
     assert response.status_code == 302  # when unauthorized user access to the admin page, it redirects to a login page
 
 
-def test_an_admin_view(admin_client):
+def test_authorized(admin_client):
     response = admin_client.get("/admin/")
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_auth_login(auto_login_user):
+    client, user = auto_login_user()
+    response = client.get("/admin/")
     assert response.status_code == 200
 
 
@@ -92,13 +110,13 @@ def test_registration_short_username_api(client):
 
 
 @pytest.mark.django_db
-def test_login_api(admin_client, register_user):
+def test_login_api(client, register_user):
     register_user(username="sang")
     payload = {
         "username": "sang",
         "password": "wjddk12wjddk",
     }
-    response = admin_client.post(
+    response = client.post(
         "http://localhost:8000/authentication/login/", data=json.dumps(dict(payload)), content_type="application/json"
     )
     assert response.status_code == 200
@@ -106,13 +124,13 @@ def test_login_api(admin_client, register_user):
 
 
 @pytest.mark.django_db
-def test_login_not_matched_payload_api(admin_client, register_user):
+def test_login_not_matched_payload_api(client, register_user):
     register_user(username="sang")
     payload = {
         "username": "sang",
         "password": "d",
     }
-    response = admin_client.post(
+    response = client.post(
         "http://localhost:8000/authentication/login/", data=json.dumps(dict(payload)), content_type="application/json"
     )
     assert response.status_code == 400
@@ -120,11 +138,41 @@ def test_login_not_matched_payload_api(admin_client, register_user):
 
 
 @pytest.mark.django_db
-def test_login_invalid_payload_api(admin_client, register_user):
+def test_login_invalid_payload_api(client, register_user):
     register_user(username="sang")
     payload = {}
-    response = admin_client.post(
+    response = client.post(
         "http://localhost:8000/authentication/login/", data=json.dumps(dict(payload)), content_type="application/json"
     )
     assert response.status_code == 400
     assert response.json() == {"username": ["This field is required."], "password": ["This field is required."]}
+
+
+@pytest.fixture
+def api_client():
+    from rest_framework.test import APIClient
+
+    return APIClient()
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "username, password, status_code",
+    [
+        (None, None, 400),
+        (None, "wjddk12wjddk", 400),
+        ("sanghun", None, 400),
+        ("sanghun", "wjddk12wjddk", 200),
+    ],
+)
+def test_login_parametrizing_api(api_client, username, password, status_code, register_user):
+    register_user(username="sanghun")
+    payload = {
+        "username": username,
+        "password": password,
+    }
+    response = api_client.post(
+        "http://localhost:8000/authentication/login/", data=json.dumps(dict(payload)), content_type="application/json"
+    )
+    print(response.json())
+    assert response.status_code == status_code
