d007b1646c9b1ce939cde785b7fbbadca7ad3e1f
sanghunjo921 <salr921@gmail.com>
2023-05-11 09:33:42 +0900

    combined fixtures for getting user info

--- realtor/test/user_tests/conftest.py
+++ realtor/test/user_tests/conftest.py
@@ -4,26 +4,20 @@ import pytest
 
 
 @pytest.fixture
-def test_password():
-    return "strong-test-pass"
+def auth_info_preset():
+    return {
+        "password": "strong-test-pass",
+        "phone": "01000000000",
+        "date_joined": "2023-05-08T22:55:10.837518Z",
+    }
 
 
 @pytest.fixture
-def test_phone():
-    return "01000000000"
-
-
-@pytest.fixture
-def test_date():
-    return "2023-05-08T22:55:10.837518Z"
-
-
-@pytest.fixture
-def create_user(db, django_user_model, test_password, test_phone, test_date):
+def create_user(db, django_user_model, auth_info_preset):
     def make_user(**kwargs):
-        kwargs["password"] = test_password
-        kwargs["phone"] = test_phone
-        kwargs["date_joined"] = test_date
+        kwargs["password"] = auth_info_preset["password"]
+        kwargs["phone"] = auth_info_preset["phone"]
+        kwargs["date_joined"] = auth_info_preset["date_joined"]
         if "username" not in kwargs:
             kwargs["username"] = str(uuid.uuid4())
         return django_user_model.objects.create_user(**kwargs)
